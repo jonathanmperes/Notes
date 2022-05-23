ea016e988c0f56811a31cc812a41d44314b506fc
jonathanmperes <emailmarceloperes@gmail.com>
2022-05-23 18:03:27 -0300

    Adicionando notas na lista e refatorando código

---
+++ app/src/main/java/br/com/app/notes/ui/activity/FormularioNotaActivity.java
@@ -0,0 +1,14 @@
+package br.com.app.notes.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormularioNotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_nota);
+    }
+}
---
+++ app/src/main/java/br/com/app/notes/ui/activity/NotaAcivityConstantes.java
@@ -0,0 +1,2 @@
+package br.com.app.notes.ui.activity;public interface NotaAcivityConstantes {
+}
